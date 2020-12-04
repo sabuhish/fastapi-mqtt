d37d4c4b2f90b6868da0d95301651fcc91d170b6
Pawel Rapkiewicz <pawel.rapkiewicz@gmail.com>
2020-12-04 19:47:29 +0100

    Fix setuptools problem with gmqtt dependencies

--- fastapi_mqtt/__init__.py
+++ fastapi_mqtt/__init__.py
@@ -1,8 +1,3 @@
-
-from fastapi_mqtt.fastmqtt import FastMQTT
-from fastapi_mqtt.config import  MQQTConfig
-
-
 __author__ = "Sabuhi Shukurov"
 
 __email__ = 'sabuhi.shukurov@gmail.com'
@@ -11,5 +6,9 @@ credits = ["Sabuhi Shukurov","Hasan Aliyev", "Tural Muradov"]
 
 __version__ = "0.0.5"
 
+from sys import modules as imported_modules
+if not "setuptools" in imported_modules.keys():
+    from fastapi_mqtt.fastmqtt import FastMQTT
+    from fastapi_mqtt.config import  MQQTConfig
 
-__all__ = ["FastMQTT", "MQQTConfig"]
+    __all__ = ["FastMQTT", "MQQTConfig"]
