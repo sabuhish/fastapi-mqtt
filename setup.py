7beb8af24de50aae236ff88cabe3478e7dafc023
Marcel Blöcher <3429578+mblo@users.noreply.github.com>
2021-09-24 14:27:45 +0200

    update setup.py to ship typing information

--- setup.py
+++ setup.py
@@ -29,6 +29,8 @@ setuptools.setup(
     install_requires=["gmqtt>=0.6.8","uvicorn>=0.12.2", 'pydantic>=1.7.2',"fastapi>=0.61.2"],
     platforms=['any'],
     packages=setuptools.find_packages(),
+    package_data={"fastapi-mqtt": ["py.typed"]},
+    zip_safe=False,
     download_url="https://github.com/sabuhish/fastapi-mqtt",
     classifiers=CLASSIFIERS,
     python_requires='>=3.6',
