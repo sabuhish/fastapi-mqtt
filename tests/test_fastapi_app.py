0a8168050e2330d45562f224c36ed3db6d84e36c
Eugenio Panadero Maciá <eugenio.panadero@gmail.com>
2024-01-03 11:01:15 +0100

    🎨 Add more typing to tests

--- tests/test_fastapi_app.py
+++ tests/test_fastapi_app.py
@@ -3,16 +3,16 @@ import asyncio
 from async_asgi_testclient import TestClient
 
 
-async def test_example_app(client: TestClient):
-    response = await client.post("/test-publish")
+async def test_example_app(app_client: TestClient):
+    response = await app_client.post("/test-publish")
     assert response.status_code == 200
     assert response.json() == {"result": True, "message": "Published"}
 
     # wait a bit for the published messages to be received and processed
-    await asyncio.sleep(0.05)
+    await asyncio.sleep(0.1)
 
     # check status of received and processed msgs
-    response = await client.get("/test-status")
+    response = await app_client.get("/test-status")
     assert response.status_code == 200
     assert response.json() == {
         "received_msgs": {"fastapi-mqtt": 1, "mqtt/test/humidity": 1, "mqtt/test/temperature": 1},
@@ -20,19 +20,19 @@ async def test_example_app(client: TestClient):
         "num_subscriptions": 2,
     }
 
-    response = await client.post("/test-unsubscribe")
+    response = await app_client.post("/test-unsubscribe")
     assert response.status_code == 200
     assert response.json() == {"result": True, "message": "Unsubscribed"}
 
-    await asyncio.sleep(0.025)
+    await asyncio.sleep(0.1)
 
-    response = await client.post("/test-publish")
+    response = await app_client.post("/test-publish")
     assert response.status_code == 200
     assert response.json() == {"result": True, "message": "Published"}
 
-    await asyncio.sleep(0.05)
+    await asyncio.sleep(0.1)
 
-    response = await client.get("/test-status")
+    response = await app_client.get("/test-status")
     assert response.status_code == 200
     assert response.json() == {
         "received_msgs": {"fastapi-mqtt": 1, "mqtt/test/humidity": 2, "mqtt/test/temperature": 1},
@@ -40,13 +40,13 @@ async def test_example_app(client: TestClient):
         "num_subscriptions": 1,
     }
 
-    response = await client.post("/test-reset")
+    response = await app_client.post("/test-reset")
     assert response.status_code == 200
     assert response.json() == {"result": True, "message": "Cleaned"}
 
-    await asyncio.sleep(0.05)
+    await asyncio.sleep(0.1)
 
-    response = await client.get("/test-status")
+    response = await app_client.get("/test-status")
     assert response.status_code == 200
     assert response.json() == {
         "received_msgs": {"fastapi-mqtt": 1, "mqtt/test/humidity": 3, "mqtt/test/temperature": 1},
